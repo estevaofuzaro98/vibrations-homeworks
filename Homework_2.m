4068e4c17b64691a939d15f89dfbdc2a88c55319
Estevão <82242138+estevaofuzaro98@users.noreply.github.com>
2021-04-15 00:53:03 -0300

    Update Homework_2.m

--- Homework_2.m
+++ Homework_2.m
@@ -25,35 +25,34 @@ wd = wn*sqrt(1-z.^2); %[rad/s]
 c = z*2*sqrt(k*m); %[Ns/m]
 
 %% 1º METODO PARA ESTIMAR AMORTECIMENTO - DOMINIO DO TEMPO
-% OBTENCAO DE h(t) E DO ENVELOPE
-ht = []; env = []; % Criando os vetores
+% OBTENCAO DE h(t)
+ht = []; % Criando os vetores
 for st=1:3
     ht = [ht; 1/(m*wd(st))*exp(-z(st)*wn*t).*sin(wd(st)*t)]; %#ok<*AGROW>
-    env = [env; 1/(m*wd(st))*exp(-z(st)*wn*t)];
 end
 % APLICANDO TRANSFORMADA DE HILBERT
-figure(figc); figc = figc + 1;
+figure(figc); figc = figc + 1; X = [];
 for st=1:3
     subplot(2,3,st)
-    X = hilbert(ht(st,:)); % HILBERT
+    X(st,:) = hilbert(ht(st,:)); % HILBERT
     if st==1
         tp = t(5000:45000); % PARTE DO TEMPO
-        Xp = abs(X(5000:45000)); % PARTE DA TRANSFORMADA
-        plot(t,log(abs(X)),'m','linewidth', 2), hold on
+        Xp = X(st,5000:45000); % PARTE DA TRANSFORMADA
+        plot(t,log(abs(X(st,:))),'m','linewidth', 2), hold on
         plot(tp,log(abs(Xp)),'r','linewidth', 2.2), hold on
         xlim([0 100])
     elseif st==2
         tp = t(500:4500); % PARTE DO TEMPO
-        Xp = abs(X(500:4500)); % PARTE DA TRANSFORMADA
-        plot(t,log(abs(X)),'k','linewidth', 2), hold on
+        Xp = abs(X(st,500:4500)); % PARTE DA TRANSFORMADA
+        plot(t,log(abs(X(st,:))),'k','linewidth', 2), hold on
         plot(tp,log(abs(Xp)),'r','linewidth', 2.2), hold on
         xlim([0 100])
     else
         tp = t(50:320); % PARTE DO TEMPO
-        Xp = abs(X(50:320)); % PARTE DA TRANSFORMADA
-        plot(t,log(abs(X)),'b','linewidth', 2), hold on
+        Xp = X(st,50:320); % PARTE DA TRANSFORMADA
+        plot(t,log(abs(X(st,:))),'b','linewidth', 2), hold on
         plot(tp,log(abs(Xp)),'r','linewidth', 2.2), hold on
-        xlim([0 100])    
+        xlim([0 100])
     end
     xlabel('$t$ [s]')
     ylabel('$\ln \left| \mathcal{H}\left[h(t)\right] \right|$')
@@ -64,23 +63,22 @@ for st=1:3
 end
 for st=1:3
     subplot(2,3,st+3)
-    X = hilbert(ht(st,:)); % HILBERT
     if st==1
         tp = t(5000:45000); % PARTE DO TEMPO
-        Xp = abs(X(5000:45000)); % PARTE DA TRANSFORMADA
-        plot(t,log(abs(X)),'m','linewidth', 2), hold on
+        Xp = X(st,5000:45000); % PARTE DA TRANSFORMADA
+        plot(t,log(abs(X(st,:))),'m','linewidth', 2), hold on
         plot(tp,log(abs(Xp)),'r','linewidth', 2.2), hold on
         xlim([0 50])
     elseif st==2
         tp = t(500:4500); % PARTE DO TEMPO
-        Xp = abs(X(500:4500)); % PARTE DA TRANSFORMADA
-        plot(t,log(abs(X)),'k','linewidth', 2), hold on
+        Xp = X(st,500:4500); % PARTE DA TRANSFORMADA
+        plot(t,log(abs(X(st,:))),'k','linewidth', 2), hold on
         plot(tp,log(abs(Xp)),'r','linewidth', 2.2), hold on
         xlim([0 5])
     else
         tp = t(50:320); % PARTE DO TEMPO
-        Xp = abs(X(50:320)); % PARTE DA TRANSFORMADA
-        plot(t,log(abs(X)),'b','linewidth', 2), hold on
+        Xp = X(st,50:320); % PARTE DA TRANSFORMADA
+        plot(t,log(abs(X(st,:))),'b','linewidth', 2), hold on
         plot(tp,log(abs(Xp)),'r','linewidth', 2.2), hold on
         xlim([0 0.5])    
     end
@@ -92,7 +90,7 @@ for st=1:3
     set(gca,'fontsize',18)
     % ESTIMANDO ZETA PELO TEMPO
         grad = polyfit(tp,log(Xp),1);
-        z_est_time(st) = -grad(1)/wn; %#ok<*SAGROW>
+        z_est_time(st) = -real(grad(1))/wn; %#ok<*SAGROW>
 end
 
 %% 2º METODO PARA ESTIMAR AMORTECIMENTO - DOMINIO DA FREQUENCIA
@@ -101,56 +99,61 @@ Hjw = [];
 for st=1:3
     Hjw = [Hjw; 1./(k-w.^2*m+1i*w*c(st))];
 end
-% ANGULO(X/F) PELA FREQUENCIA COM ZOOM
+% ABS(X/F) PELA FREQUENCIA COM ZOOM
 for st=1:3
+    maximum = max(abs(Hjw(st,:)));
+    maximum_sqrt2 = (max(abs(Hjw(st,:)))/sqrt(2));
     fig = figure(figc); figc = figc + 1;
-    plot(f,rad2deg(-pi/4)*ones(1,length(w)),'r--','linewidth', 1.5), hold on
-    plot(f,rad2deg(-pi/2)*ones(1,length(w)),'r--','linewidth', 1.5), hold on
-    plot(f,rad2deg(-3*pi/4)*ones(1,length(w)),'r--','linewidth', 1.5), hold on
+    plot(f,maximum*ones(1,length(w)),'r--','linewidth', 1.5), hold on
+    plot(f,maximum_sqrt2*ones(1,length(w)),'r--','linewidth', 1.5), hold on
     xlabel('$f$ [Hz]')
-    ylabel('$\phi$ [$^{\circ}$]')
+    ylabel('$\left| \frac{X}{F} \right|$')
     title(['$\zeta = ', num2str(z(st)), ' $'],'FontWeight','normal'), 
     grid on, grid minor
-    xlim([0 60])
-    set(gca,'fontsize',18,'YTick',[-180 -135 -90 -45 0])
+    set(gca,'fontsize',18)
     if st==1
-        plot(f,rad2deg(angle(Hjw(st,:))),'m','linewidth', 2.5), hold on
+        xlim([15 19])
+        plot(f,abs(Hjw(st,:)),'m','linewidth', 2.5), hold on
         xlm = [15.89,15.94];
-        ylm = [-140,-40];
-        pos = [0.50 0.20 0.38 0.635];
-        annotation('arrow',[0.3555,0.5],[0.45,0.48]);
-        magnifyPlot(xlm,ylm,pos,1.8,true,xlm(1):0.01:xlm(2),[-135 -120 -105 -90 -75 -60 -45])
+        ylm = [0.95*maximum_sqrt2,1.05*maximum];
+        pos = [0.50 0.21 0.38 0.635];
+        annotation('arrow',[0.313,0.5],[0.65,0.67]);
+        magnifyPlot(xlm,ylm,pos,1.8,true,xlm(1):0.01:xlm(2),[maximum_sqrt2 maximum])
             % ESTIMANDO ZETA PELA FREQUENCIA
             z_est_freq(st) = (15.9313-15.8995)/(2*fn);
     elseif st==2
-        plot(f,rad2deg(angle(Hjw(st,:))),'k','linewidth', 2.5), hold on
+        xlim([14 24])
+        plot(f,abs(Hjw(st,:)),'k','linewidth', 2.5), hold on
         xlm = [15.7,16.1];
-        ylm = [-140,-40];
-        pos = [0.50 0.20 0.38 0.635];
-        annotation('arrow',[0.3555,0.5],[0.45,0.48]);
-        magnifyPlot(xlm,ylm,pos,1.8,true,xlm(1):0.1:xlm(2),[-135 -120 -105 -90 -75 -60 -45])
+        ylm = [0.95*maximum_sqrt2,1.05*maximum];
+        pos = [0.50 0.21 0.38 0.635];
+        annotation('arrow',[0.293,0.5],[0.65,0.67]);
+        magnifyPlot(xlm,ylm,pos,1.8,true,xlm(1):0.1:xlm(2),[maximum_sqrt2 maximum])
             % ESTIMANDO ZETA PELA FREQUENCIA
-            z_est_freq(st) = (16.0748-15.7559)/(2*fn);
+            z_est_freq(st) = (16.0721-15.7536)/(2*fn);
     else
-        plot(f,rad2deg(angle(Hjw(st,:))),'b','linewidth', 2.5), hold on
-        xlm = [14.3,17.9];
-        ylm = [-140,-40];
-        pos = [0.50 0.20 0.38 0.635];
-        annotation('arrow',[0.3555,0.5],[0.45,0.48]);
-        magnifyPlot(xlm,ylm,pos,1.8,true,xlm(1):0.4:xlm(2),[-135 -120 -105 -90 -75 -60 -45])
+        xlim([5 60])
+        plot(f,abs(Hjw(st,:)),'b','linewidth', 2.5), hold on
+        xlm = [13.85,17.45];
+        ylm = [0.95*maximum_sqrt2,1.05*maximum];
+        pos = [0.50 0.21 0.38 0.635];
+        annotation('arrow',[0.306,0.5],[0.65,0.67]);
+        magnifyPlot(xlm,ylm,pos,1.8,true,xlm(1):0.6:xlm(2),[maximum_sqrt2 maximum])
             % ESTIMANDO ZETA PELA FREQUENCIA
-            z_est_freq(st) = (17.5755-14.3977)/(2*fn);
+            z_est_freq(st) = (17.2823-14.0571)/(2*fn);
     end
 end
 
-%% COMPARACAO ENTRE OS METODOS DO TEMPO E DA FREQUENCIA
-perc_error_time = abs(z_est_time - z)./z*100;
-perc_error_freq = abs(z_est_freq - z)./z*100;
-
+%% COMPARACAO ENTRE OS METODOS
+for st=1:3
+    perc_error_time(st) = (abs(z_est_time(st) - z(st))/z(st))*100;
+    perc_error_freq(st) = (abs(z_est_freq(st) - z(st))/z(st))*100;
+end
 fprintf('Zeta estimados pelo tempo: %f \n', z_est_time)
-fprintf('\n')
-fprintf('Erros percentuais pelo tempo: %f \n', perc_error_time)
 fprintf('\n')
 fprintf('Zeta estimados pela frequencia: %f \n', z_est_freq)
 fprintf('\n')
+fprintf('Erros percentuais pelo tempo: %f \n', perc_error_time)
+fprintf('\n')
 fprintf('Erros percentuais pela freq: %f \n', perc_error_freq)
+fprintf('\n')
